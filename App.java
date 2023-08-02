2f7fb1f3fc30fcc665deae3947ebe36b3f1a146b
ShadraG011 <vgardash21@gmail.com>
2023-08-02 10:57:06 +1000

    change file App.java

--- App.java
+++ App.java
@@ -4,6 +4,7 @@ class App {
      */
     public static void main(String[] args) {
         Run run = new Run();
+        System.out.println("Программы работает в штатном режиме");
         run.buttonRun();
     }    
 }
